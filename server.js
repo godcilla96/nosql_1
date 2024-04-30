6954ba5765c956c000c07d73233ddcf8ef1f5bde
Cecilia Lindström <cecilia.a.lindstrom@live.se>
2024-04-30 12:18:23 +0200

    schema + påbörjat routing

--- server.js
+++ server.js
@@ -15,3 +15,52 @@ mongoose.connect("mongodb://127.0.0.1:27017/cv").then(() => {
     console.log("error connecting to the database: " + error);
 })
 
+//workexperience schema
+
+const ExperienceSchema = new mongoose.Schema({
+    companyname: {
+        type: String,
+        required: [true, "Du måste ange företagsnamnet"],
+    },
+    jobtitle: {
+        type: String,
+        required: [true, "Du måste ange jobbtitel"],
+    },
+    location: {
+        type: String,
+        required: [true, "Du måste ange ort"],
+    },
+    startdate: {
+        type: Date,
+        required: [true, "Du måste ange startdatum"],
+    },
+    enddate: {
+        type: Date,
+        required: [true, "Du måste ange slutdatum"],
+    },
+    description: {
+        type: String,
+        required: [true, "Du måste ange beskrivning"],
+    }
+})
+
+//inkludera schemat till databasen
+const Experience = mongoose.model("Workexperience", ExperienceSchema);
+
+//routing
+app.get('/api', async (req, res) => {
+    res.json({message: "welcome to this API"});
+});
+
+app.get("/workexperience", async (req, res) => {
+    try {
+        let result = await Experience.create(req.body);
+        return res.json(result);
+    }catch(error) {
+        return res.status(500).json(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log("Server startad på port: " + port);
+});
